c0ba9c9293f72ae0d38f85f5b2b99abf2aefa4d4
gmontantez <gmontantez7@gmail.com>
2018-02-20 10:58:10 -0500

    Adding Exercise

--- adding_exercise.rb
+++ adding_exercise.rb
@@ -1,5 +1,19 @@
 def addition(x,y)
 	puts x + y
+	x + y
 end
 
- 	addition(60,20)
+def compare(a,b)
+	if a == b 
+		print "pass"
+	else 
+		print "fail"
+	end
+end
+
+ 	#addition(60,20)
+
+	#compare(10,"20".to_i) 
+	#compare("true","true")
+	#compare(15,10+5)
+	compare(10,addition(2,8))
